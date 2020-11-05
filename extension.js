91777b4aacae90ea5a27a6f5b39c3ee795e726f6
Thomas Moerbauer <tmoer93@gmail.com>
2020-11-05 18:35:45 +0100

    Removed the panelbarrier handling to avoid shell crashes

--- extension.js
+++ extension.js
@@ -1,46 +1,18 @@
 
 const Main = imports.ui.main;
-const Meta = imports.gi.Meta;
 const PanelBox = Main.layoutManager.panelBox;
 
 let MonitorsChangedListener = null;
 let HeightNotifyListener = null;
-let rightPanelBarrier = null;
 
 function _toTop() {
-   let monitor = Main.layoutManager.primaryMonitor; 
-   if (this.rightPanelBarrier) {
-        this.rightPanelBarrier.destroy()
-   }
-   this.rightPanelBarrier = new Meta.Barrier({
-       display: global.display,
-       x1: monitor.width, 
-       x2: monitor.width, 
-       y1: PanelBox.height, 
-       y2: 0, 
-       directions: Meta.BarrierDirection.NEGATIVE_Y
-   });    
+   let monitor = Main.layoutManager.primaryMonitor;  
    PanelBox.set_pivot_point(0,0);
    PanelBox.set_position(0,0);
 }
 
 function _toBottom() {
     let monitor = Main.layoutManager.primaryMonitor;
-    if (this.rightPanelBarrier) {
-        this.rightPanelBarrier.destroy()
-    }
-    this.rightPanelBarrier = new Meta.Barrier({
-        display: global.display, 
-        x1: monitor.width, 
-        x2: monitor.width, 
-        y1: monitor.height - PanelBox.height, 
-        y2: monitor.height,
-        directions: Meta.BarrierDirection.NEGATIVE_Y
-    });
-    // TODO: find a way to replace the rightPanelBarrier instead of destroying
-    if (Main.layoutManager._rightPanelBarrier) {
-        Main.layoutManager._rightPanelBarrier.destroy()
-    }
     PanelBox.set_pivot_point(0,(-1)*(monitor.height-PanelBox.height));
     PanelBox.set_position(0,(monitor.height-PanelBox.height));
 }
